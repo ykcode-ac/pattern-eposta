a282dfb8ea4bdd1585feeec2fa2ef2f5ac605e3c
ahmetc <ahmetc@MSIAC>
2024-12-19 18:48:25 +0300

    Add project files.

---
+++ pattern-eposta/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace pattern_eposta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnGiris = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(111, 44);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(251, 26);
+            this.txtEmail.TabIndex = 1;
+            // 
+            // btnGiris
+            // 
+            this.btnGiris.Location = new System.Drawing.Point(111, 76);
+            this.btnGiris.Name = "btnGiris";
+            this.btnGiris.Size = new System.Drawing.Size(251, 57);
+            this.btnGiris.TabIndex = 2;
+            this.btnGiris.Text = "Giriş";
+            this.btnGiris.UseVisualStyleBackColor = true;
+            this.btnGiris.Click += new System.EventHandler(this.btnGiris_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 311);
+            this.Controls.Add(this.btnGiris);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnGiris;
+    }
+}
+
---
+++ pattern-eposta/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pattern_eposta
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGiris_Click(object sender, EventArgs e)
+        {
+            // Kullanıcıdan alınan e-posta adresi
+            string email = txtEmail.Text;
+
+            // Hata mesajlarını tutmak için string değişken
+            string errorMessage = "";
+
+            // Regex desenini güncelleme
+            string userNamePattern = @"^[a-zA-Z0-9._-]{6,}";  // Kullanıcı adı en az 6 karakter olmalı
+            string domainPattern = @"@[a-zA-Z0-9-]{2,}";      // Domain en az 2 karakter olmalı
+            string extensionPattern = @"\.[a-zA-Z]{2,}$";      // Uzantı en az 2 karakter olmalı
+
+            // Kullanıcı adı kontrolü
+            if (!Regex.IsMatch(email, userNamePattern))
+            {
+                errorMessage += "- Kullanıcı adı en az 6 karakter olmalı.\n";
+            }
+
+            // Domain kontrolü
+            if (!Regex.IsMatch(email, domainPattern))
+            {
+                errorMessage += "- Domain en az 2 karakter olmalı.\n";
+            }
+
+            // Uzantı kontrolü
+            if (!Regex.IsMatch(email, extensionPattern))
+            {
+                errorMessage += "- Uzantı en az 2 karakter olmalı.\n";
+            }
+
+            // Eğer herhangi bir hata varsa, mesaj göster
+            if (errorMessage != "")
+            {
+                MessageBox.Show("Lütfen geçerli bir e-posta adresi girin:\n\n" + errorMessage,
+                    "Hata",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show("E-posta adresi geçerlidir.", "Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
